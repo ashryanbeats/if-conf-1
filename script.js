d1d67f3873c4d5b64d4fc0a0166592016f78cb70
Ash Ryan <ash@ashryanbeats.com>
2015-09-13 16:32:17 -0400

    Basic language toggling.

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-!function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);
+$(document).ready(function() {
+  // Initialize Localizejs
+  !function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);
 
-Localize.initialize({
-  key: 'jwfTGgB1vDG0b',
-  rememberLanguage: true
+  Localize.initialize({
+    key: 'jwfTGgB1vDG0b',
+    rememberLanguage: true
+  });
+  //
+
+  $('#lang-en').hide();
+
+  $('#lang-ja').click(function() {
+    toggleLang();
+    Localize.setLanguage('ja');
+  });
+
+  $('#lang-en').click(function() {
+    toggleLang();
+    Localize.setLanguage('en');
+  });
+
+  function toggleLang() {
+    $('#lang-en').toggle();
+    $('#lang-ja').toggle();
+  }
+
 });
