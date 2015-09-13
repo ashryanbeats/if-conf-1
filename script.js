28d40fa763393f7ba8b6487d64fd67872f23b0a4
Ash Ryan <ash@ashryanbeats.com>
2015-09-13 16:08:17 -0400

    Basic language switcher.

---
+++ script.js
@@ -0,0 +1,6 @@
+!function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);
+
+Localize.initialize({
+  key: 'jwfTGgB1vDG0b',
+  rememberLanguage: true
+});
